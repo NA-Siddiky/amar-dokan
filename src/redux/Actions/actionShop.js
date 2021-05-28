c5e2ac273de599d47e5ee5a818fb45fcd024032f
NA-Siddiky <siddiky.academic@gmail.com>
2021-05-28 18:52:11 +0600

    1st bug fix

--- src/redux/Actions/actionShop.js
+++ src/redux/Actions/actionShop.js
@@ -1,6 +1,6 @@
 export const addBook = (book) => {
     return {
-        type: "ADD_TO_READING_LIST",
-        payload: book,
+        type: ,
+        payload:,
     };
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { combineReducers, createStore } from "redux";
-import reducerBooks from "./reducers/reducerBooks";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
-  books: reducerBooks,
+  
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
