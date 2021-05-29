0e703863ac442f905815f67a8e5a8396bfc2fafa
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-05-29 17:02:51 +0600

    Cetagory and product section done

--- src/components/HomePage/Home/Home.js
+++ src/components/HomePage/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Category from '../Category/Category';
 import Header from '../Header/Header';
 import Navbar from '../Navbar/Navbar';
 import NewArrivals from '../NewArrivals/NewArrivals';
+import Product from '../Product/Product';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Header></Header>
-            <NewArrivals></NewArrivals>
+            <Category></Category>
+            <Product></Product>
+            {/* <NewArrivals></NewArrivals> */}
         </div>
     );
 };
