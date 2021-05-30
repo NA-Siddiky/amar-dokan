9634e9815a39ba7d87bad6808b1a96f78d4b817b
NA-Siddiky <siddiky.academic@gmail.com>
2021-05-30 20:52:14 +0600

    login done

---
+++ src/Pages/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCZ6NavFZ0uUDSt0RT_WlG8oJCyGuN5jw",
+    authDomain: "amar-dokan-ff3c9.firebaseapp.com",
+    projectId: "amar-dokan-ff3c9",
+    storageBucket: "amar-dokan-ff3c9.appspot.com",
+    messagingSenderId: "517172823444",
+    appId: "1:517172823444:web:842339d133a593ca547b81",
+    measurementId: "G-FBCWRHDF6G"
+};
+export default firebaseConfig;
